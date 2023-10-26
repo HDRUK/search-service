68d9ccfbe281b80f0007213ad702c642182e19c1
Branwen Snelling <branwen.snelling@hdruk.ac.uk>
2023-10-26 10:56:08 +0100

    rm commented code and env

--- pkg/search.go
+++ pkg/search.go
@@ -12,35 +12,6 @@ import (
 	"hdruk/search-service/utils/elastic"
 )
 
-// Loads environment variables and defines the ElasticSearch client
-// Authentication and elastic deployment endpoint are required environment variables
-// func init() {
-// 	err := godotenv.Load("../.env")
-// 	if err != nil {
-// 		log.Fatal(err.Error())
-// 	}
-
-// 	// Note: we might not need to define custom transport with infra hosted elastic
-// 	// It is defined here in order to disable SSL cert verification
-// 	tr := &http.Transport{
-//         TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-//     }
-// 	clusterURLs := []string{os.Getenv("ELASTIC_URL")}
-// 	username := os.Getenv("ELASTIC_USERNAME")
-// 	password := os.Getenv("ELASTIC_PASSWORD")
-
-// 	es, err := elasticsearch.NewClient(elasticsearch.Config{
-// 		Addresses: clusterURLs,
-// 		Username: username,
-// 		Password:  password,
-// 		Transport: tr,
-// 	})
-// 	if err != nil {
-// 		log.Fatal(err.Error())
-// 	}
-// 	elasticClient = es
-// }
-
 // Query represents the search query incoming from the gateway-api
 // Expected to be a string in the body of the request e.g. {'query': 'diabetes snomed'}
 type Query struct {
