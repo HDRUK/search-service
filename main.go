ff27fb8bc79464a3b11896dad4ffb43fe9802c13
Calum Macdonald <calmacx@gmail.com>
2024-02-21 09:40:17 +0000

    missed mappings

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func main() {
 	router.POST("/settings/tools", search.DefineToolSettings)
 	router.POST("/settings/collections", search.DefineCollectionSettings)
 	router.POST("/mappings/datasets", search.DefineDatasetMappings)
+	router.POST("/mappings/collections", search.DefineCollectionMappings)
 	router.POST("/filters", search.ListFilters)
 	router.POST("/similar/datasets", search.SearchSimilarDatasets)
 
