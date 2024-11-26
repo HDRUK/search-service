86edfce7cc042a0fc795c784fa02475e89d3d6a6
Dan Nita <dan.nita@hdruk.ac.uk>
2024-11-26 11:04:12 +0000

    update

--- pkg/settings.go
+++ pkg/settings.go
@@ -789,7 +789,7 @@ func DefineDataCustodianNetworkMappings(c *gin.Context) {
 	elasticMappings := gin.H{
 		"mappings": gin.H{
 			"properties": gin.H{
-				"publisherName":    gin.H{"type": "keyword"},
+				"publisherNames":   gin.H{"type": "keyword"},
 				"datasetTitles":    gin.H{"type": "keyword"},
 				"durTitles":        gin.H{"type": "keyword"},
 				"toolNames":        gin.H{"type": "keyword"},
