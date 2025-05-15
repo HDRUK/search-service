7524d5d6b1074796c63f10ab30702689bbc14400
BSnelling <branwen.snelling@hdruk.ac.uk>
2025-05-15 11:36:13 +0100

    feat(GAT-6872): Add big query upload function for search analysis data (#160)

---
+++ utils/bigquery/client.go
@@ -0,0 +1,19 @@
+package bigqueryclient
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"cloud.google.com/go/bigquery"
+)
+
+func DefaultBigQueryClient() *bigquery.Client {
+	projectID := os.Getenv("BQ_PROJECT_ID")
+	ctx := context.Background()
+	client, err := bigquery.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return client
+}
